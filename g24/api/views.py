81caaac69395f2a1129f99fd7146e94c8f600276
nagar1088 <sachinnagar094@gmail.com>
2024-04-05 17:33:28 +0530

    token commit

---
+++ g24/api/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import student
+from .serializers import StudentSerializers
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+class StudentModelViewSet(viewsets.ModelViewSet):
+    queryset=student.object.all()
+    serializer_class=StudentSerializers
+    # authentication_classes=[SessionAuthenticationAuthentication]
+    # permission_classes=[IsAuthenticated]
+   
+
